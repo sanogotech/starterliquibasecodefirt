dc24583d96c57642b89a35f61d42b846433546be
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2023-10-15 06:48:29 +0000

    Update

---
+++ src/main/java/com/macrosoft/myliquidbasestarter/MyliquidbasestarterApplication.java
@@ -0,0 +1,13 @@
+package com.macrosoft.myliquidbasestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyliquidbasestarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyliquidbasestarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macrosoft/myliquidbasestarter/MyliquidbasestarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macrosoft.myliquidbasestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyliquidbasestarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1c25e8b46193d66baecaa1c3a75ad25f2f0ed925
SANOGOTECH <sanogotech@yahoo.fr>
2023-10-15 07:01:41 +0000

    Create Student.java

---
+++ src/main/java/com/macrosoft/myliquidbasestarter/model/Student.java
@@ -0,0 +1,57 @@
+package com.macrosoft.myliquidbasestarter.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Le nom ne peut pas être vide")
+    @Size(max = 100, message = "Le nom ne peut pas dépasser 100 caractères")
+    private String name;
+
+    @NotBlank(message = "Le niveau ne peut pas être vide")
+    @Size(max = 50, message = "Le niveau ne peut pas dépasser 50 caractères")
+    private String grade;
+
+
+    // Getters et setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+
+    
+    
+
+   
+}
